d1f46d9cd4f40b6203dede0f9f05d04374a380cd
Sam Nicholson <sam.nicholson@infinityworks.com>
2022-03-08 10:00:21 +0000

    Initializr project setup
    Initial project structure from spring initializr.

---
+++ src/main/java/snicholson/featureflag/FeatureflagApplication.java
@@ -0,0 +1,13 @@
+package snicholson.featureflag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureflagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureflagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/snicholson/featureflag/FeatureflagApplicationTests.java
@@ -0,0 +1,13 @@
+package snicholson.featureflag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureflagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
